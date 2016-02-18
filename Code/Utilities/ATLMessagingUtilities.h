07b52626d7580e0703092bf35f4f7d679fcac395
Kabir Mahal <kabirmahal@gmail.com>
2016-02-17 17:03:57 -0800

    Added identity support to Atlas and mocks

--- Code/Utilities/ATLMessagingUtilities.h
+++ Code/Utilities/ATLMessagingUtilities.h
@@ -84,6 +84,12 @@ CGRect ATLImageRectConstrainedToSize(CGSize imageSize, CGSize maxSize);
 
 CGFloat ATLDegreeToRadians(CGFloat degrees);
 
+//-----------------------------
+// @name Conversation Utilities
+//-----------------------------
+
+LYRIdentity *__nullable ATLIdentityFromSet(NSString *userID, NSSet *participants);
+
 //------------------------
 // @name Message Utilities
 //------------------------
